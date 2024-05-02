05292ed3d89e5d6c3a98c6e8548ca6c7044cde91
pramodtoleti47 <pramodh.t@eficens.com>
2024-05-02 21:33:13 +0530

    Add routes for posts in posts.routes.js

---
+++ routes/posts.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import multer from "multer";
+import * as postsController from "../controllers/posts.controller";
+
+const router = Router();
+const upload = multer({
+  dest: "uploads/",
+});
+
+router.get("/", postsController.getPosts);
+router.get("/:id", postsController.getPost);
+router.post("/", upload.single("image"), postsController.createPost);
+router.put("/:id", postsController.updatePost);
+router.delete("/:id", postsController.deletePost);
