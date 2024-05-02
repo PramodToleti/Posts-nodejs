a7f32b944b32c5ec6c825afe1ad620df8211f5fc
pramodtoleti47 <pramodh.t@eficens.com>
2024-05-02 21:32:50 +0530

    Add admin configuration file for database and cloudinary

---
+++ config/admin.js
@@ -0,0 +1,30 @@
+import { Sequelize } from "sequelize";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+//Database Connection. Sequelize Docs: https://sequelize.org/docs/v6/core-concepts/model-basics/
+export const sequelize = new Sequelize({
+  host: process.env.MYSQL_HOST,
+  dialect: process.env.MYSQL_DIALECT,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+//Cloudinary Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const connect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
